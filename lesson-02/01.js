8a9af9ef9097fe4a765caf62c783562973ade0cb
Starkov Sergey <starkovsr@gmail.com>
2024-06-03 17:28:43 +0300

    add tasks for lessons 2

---
+++ lesson-02/01.js
@@ -0,0 +1,21 @@
+// Создать программный код, который определяет, может ли пользователь получить доступ к ресурсу на основе его роли и наличия разрешений.
+// Полученное значение сохраните в переменной isAccess.
+
+// Условия доступа:
+// - Пользователь должен иметь статус администратора (**`isAdmin`**) или быть подтвержденным пользователем (**`isVerifiedUser`**)tr
+// - А также пользователь должен обладать специальным разрешением (**`hasSpecialPermission`**) или временным пропуском (**`hasTemporaryPass`**)
+
+// тестовые данные (значения можно менять)
+const isAdmin = false
+const isVerifiedUser = true
+const hasSpecialPermission = true
+const hasTemporaryPass = false
+
+let isAccess
+
+// ваш код
+if ((isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass)) {
+  isAccess = true
+} else {
+  isAccess = false
+}
---
+++ lesson-02/03.js
@@ -0,0 +1,20 @@
+/*
+ * Напишите код, который будет определять оценку студента (grade) в зависимости от балла (score).
+ * Балл может быть от 0 до 100, а оценки F (0 - 49), D (50 - 69), C (70-69), B (80 - 89), A (90 - 100)
+ */
+
+let score = 70 // тестовое значение, можно изменять
+let grade
+
+// ваш код
+if (score >= 0 && score <= 49) {
+  grade = 'F'
+} else if (score >= 50 && score <= 69) {
+  grade = 'D'
+} else if (score >= 70 && score <= 79) {
+  grade = 'C'
+} else if (score >= 80 && score <= 89) {
+  grade = 'B'
+} else if (score >= 90 && score <= 100) {
+  grade = 'A'
+}
