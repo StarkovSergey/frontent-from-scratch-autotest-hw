0ee7c4fb888fa2f806af3b9af300b0e5f3a12b3b
Starkov Sergey <starkovsr@gmail.com>
2024-06-05 12:07:37 +0300

    add tasts for lesson 2

---
+++ lesson-03/03.js
@@ -0,0 +1,7 @@
+// Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
+// function findLargest() {}
+
+// Решение
+function findLargest(a, b, c) {
+  return Math.max(a, b, c)
+}
